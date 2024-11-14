8cf03723532fdadf34693b56d77292cc76959da2
Joshua <Joshino784@gmail.com>
2024-11-14 08:22:06 +0000

    Basic api functionality, updated basic ejs templates

--- routes/api.js
+++ routes/api.js
@@ -1,19 +1,52 @@
 const express = require("express");
 const router = express.Router();
-const redirectLogin = require('../middleware/redirectLogin')
-
+const redirectLogin = require("../middleware/redirectLogin");
+const request = require("request");
 
 //This will be used to add the api to search for songs, the regular will list maybe like 40 songs on the page then search functionality will be added.
 
-router.get("/", redirectLogin, function (req, res, next) {
-    res.render("songs.ejs"); //placeholder
+router.get("/", function (req, res, next) {
+  // Get page, offset, and limit from query
+  const api_key = "858b19bec0a85e46c4e8cc260545295a";
+
+  const url = `https://ws.audioscrobbler.com/2.0/?method=chart.gettoptracks&api_key=${api_key}&format=json`;
+
+  request(url, function (err, response, body) {
+    if (err) {
+      next(err);
+    } else {
+      const data = JSON.parse(body);
+      const tracks = data.tracks.track;
+
+      res.render("songs.ejs", { tracks });
+    }
+  });
 });
 
+router.get("/search", function (req, res, next) {
+  const query = req.query.query;
+  const api_key = "858b19bec0a85e46c4e8cc260545295a";
+  const url = `https://ws.audioscrobbler.com/2.0/?method=track.search&track=${query}&api_key=${api_key}&format=json`;
 
+  request(url, function (err, response, body) {
+    if (err) {
+      next(err);
+    } else {
+      const data = JSON.parse(body);
+      const track = data.results.trackmatches.track;
+      const tracks = track.map((track) => ({
+        name: track.name,
+        artist: track,
+        url: track.url,
+        listeners: track.listeners // Add this if available in the API response
+      }));
 
+      console.log(tracks);
 
-
-
+      res.render("songs.ejs", { tracks });
+    }
+  });
+});
 
 // Export the router object so index.js can access it
 module.exports = router;
